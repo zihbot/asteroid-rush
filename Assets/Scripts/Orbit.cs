7c85ea6ce3d164891286c63ae41308176e5fc5e0
Botond Zih <zihbot@gmail.com>
2023-03-23 18:30:37 +0100

    calculate gravitational const

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class Orbit : MonoBehaviour
 {
+    // 1 ld = 3.844 * 10^8 m
+    // 1 em = 5.972 * 10^24 kg
+    // 1 d  = 8.640 * 10^4 s
+
+    //   6.674 * 10^−11 m * m * m / kg / s / s =
+    // = 6.674 / 3.844 / 3.844 / 3.844 * 5.972 * 8.64 * 8.64 * 10^(-11-8-8-8+24+4+4) =
+    // = 0.05238215355 ld * ld * ld / em / d / d
+    public const float ldEmDgravitationalConst = 0.05238f;
+
     public const int resolution = 720;
 
     public CelestialBody Center;
@@ -29,15 +38,15 @@ public class Orbit : MonoBehaviour
         radii = new float[resolution];
         for (int i = 0; i < resolution; i++)
         {
-            float theta = 2 * Mathf.PI * i / resolution;
-            float r = LdSemiMajorAxis * (1 - Eccentricity * Eccentricity) / (1 + Eccentricity * Mathf.Cos(theta));
+            float trueAnomaly = 2 * Mathf.PI * i / resolution;
+            float r = LdSemiMajorAxis * (1 - Eccentricity * Eccentricity) / (1 + Eccentricity * Mathf.Cos(trueAnomaly));
             if (r > 1000)
             {
                 lineRenderer.positionCount = i;
                 break;
             }
             radii[i] = r;
-            positions[i] = new Vector3(Mathf.Sin(theta), 0, Mathf.Cos(theta)) * r;
+            positions[i] = new Vector3(Mathf.Sin(trueAnomaly), 0, Mathf.Cos(trueAnomaly)) * r;
         }
 
         lineRenderer.SetPositions(positions);
