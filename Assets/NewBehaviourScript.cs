3065daaa08da8e3e51c5a2412f39e675ae5ca457
Botond Zih <zihbot@gmail.com>
2023-03-28 21:31:57 +0200

    add manager

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Orbit/Orbit")]
+public class NewBehaviourScript : ScriptableObject
+{
+    public int asd;
+}
