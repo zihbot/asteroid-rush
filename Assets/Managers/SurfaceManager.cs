a59ca27914195bf3be7c54ae80d6577f8cd66586
Botond Zih <zihbot@gmail.com>
2023-04-25 21:24:33 +0200

    surface centers

--- Assets/Managers/SurfaceManager.cs
+++ Assets/Managers/SurfaceManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SurfaceManager : MonoBehaviour
 {
+    #region Singleton
     public static SurfaceManager Instance { get; private set; }
     private void Awake()
     {
@@ -15,10 +16,30 @@ public class SurfaceManager : MonoBehaviour
         Instance = this;
         Init();
     }
+    #endregion
 
     void Init()
     {
-
+        createExample();
     }
 
+    private void createExample()
+    {
+        float distance = 0.5f;
+        float R = 2f;
+        int imax = Mathf.FloorToInt(Mathf.PI / (distance / R));
+        for (int i = 0; i <= imax; i++)
+        {
+            float theta = i * distance / R;
+            float r = (R * Mathf.Sin(theta));
+            int jmax = Mathf.FloorToInt(2 * Mathf.PI * r / distance);
+            for (int j = 0; j < jmax; j++)
+            {
+                float phi = j * distance / r;
+                GameObject point = new GameObject("Point");
+                point.transform.position = new Vector3(r * Mathf.Cos(phi), (R * Mathf.Cos(theta)), r * Mathf.Sin(phi));
+            }
+        }
+    }
 }
+
