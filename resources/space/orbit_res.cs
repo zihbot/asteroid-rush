1486ff944b51d53c52fd27b9af9aaae4c7d69252
Botond Zih <zihbot@gmail.com>
2023-10-16 20:58:26 +0200

    Add orbit res

---
+++ resources/space/orbit_res.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class OrbitRes : Resource
+{
+    [Export] public float SemiMajorAxis { get; set; } = 1;
+    [Export] public float Eccentricity { get; set; } = 0;
+    [Export] public float Inclination { get; set; } = 0;
+    [Export] public float LongitudeOfAscendingNode { get; set; } = 0;
+    [Export] public float ArgumentOfPeriapsis { get; set; } = 0;
+    [Export] public float MeanAnomaly { get; set; } = 0;
+
+    OrbitRes()
+    {
+    }
+}
