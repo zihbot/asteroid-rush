ac5c76432f4baf27abd6c1350ce96b070bef7d04
Botond Zih <zihbot@gmail.com>
2023-10-19 19:33:56 +0200

    formatting

--- scenes/space/orbit/OrbitDrawer.cs
+++ scenes/space/orbit/OrbitDrawer.cs
@@ -4,42 +4,41 @@ using System.Collections.Generic;
 
 public partial class OrbitDrawer : MeshInstance3D
 {
-	[Export] public OrbitData OrbitData { get; set; } = new OrbitData();
+    [Export] public OrbitData OrbitData { get; set; } = new OrbitData();
 
-	public override void _Ready()
-	{
-		OrbitData.Changed += RecalculateMesh;
-		RecalculateMesh();
-	}
+    public override void _Ready()
+    {
+        OrbitData.Changed += RecalculateMesh;
+        RecalculateMesh();
+    }
 
-	private void RecalculateMesh()
-	{
-		var surfaceArray = new Godot.Collections.Array();
-		surfaceArray.Resize((int)Mesh.ArrayType.Max);
+    private void RecalculateMesh()
+    {
+        var surfaceArray = new Godot.Collections.Array();
+        surfaceArray.Resize((int)Mesh.ArrayType.Max);
 
-		var verts = new List<Vector3>();
-		var uvs = new List<Vector2>();
-		var normals = new List<Vector3>();
-		var indices = new List<int>();
+        var verts = new List<Vector3>();
+        var uvs = new List<Vector2>();
+        var normals = new List<Vector3>();
+        var indices = new List<int>();
 
-		for (float fi = -Mathf.Pi; fi < Mathf.Pi; fi += 2 * Mathf.Pi / 360)
-		{
-			var pos = OrbitData.PositionAtTrueAnomaly(fi);
-			GD.Print(pos);
-			verts.Add(pos);
-			uvs.Add(new Vector2(0, 0));
-			normals.Add(new Vector3(0, 1, 0));
-			indices.Add(verts.Count - 1);
-		}
+        for (float fi = -Mathf.Pi; fi < Mathf.Pi; fi += 2 * Mathf.Pi / 360)
+        {
+            var pos = OrbitData.PositionAtTrueAnomaly(fi);
+            verts.Add(pos);
+            uvs.Add(new Vector2(0, 0));
+            normals.Add(new Vector3(0, 1, 0));
+            indices.Add(verts.Count - 1);
+        }
 
-		surfaceArray[(int)Mesh.ArrayType.Vertex] = verts.ToArray();
-		surfaceArray[(int)Mesh.ArrayType.TexUV] = uvs.ToArray();
-		surfaceArray[(int)Mesh.ArrayType.Normal] = normals.ToArray();
-		surfaceArray[(int)Mesh.ArrayType.Index] = indices.ToArray();
+        surfaceArray[(int)Mesh.ArrayType.Vertex] = verts.ToArray();
+        surfaceArray[(int)Mesh.ArrayType.TexUV] = uvs.ToArray();
+        surfaceArray[(int)Mesh.ArrayType.Normal] = normals.ToArray();
+        surfaceArray[(int)Mesh.ArrayType.Index] = indices.ToArray();
 
-		var arrMesh = new ArrayMesh();
-		arrMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.LineStrip, surfaceArray);
-		Mesh = arrMesh;
-	}
+        var arrMesh = new ArrayMesh();
+        arrMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.LineStrip, surfaceArray);
+        Mesh = arrMesh;
+    }
 
 }
--- scenes/space/planetary_system/CameraMovement.cs
+++ scenes/space/planetary_system/CameraMovement.cs
@@ -3,48 +3,48 @@ using System;
 
 public partial class CameraMovement : Node3D
 {
-	[Export] public Camera3D Camera = null;
-	[Export] public Vector3 Center = Vector3.Zero;
-	[Export] public float Sensitivity = 0.003f;
+    [Export] public Camera3D Camera = null;
+    [Export] public Vector3 Center = Vector3.Zero;
+    [Export] public float Sensitivity = 0.003f;
 
-	public override void _Ready()
-	{
-		Camera ??= GetViewport().GetCamera3D();
-		Camera.LookAt(Center, Vector3.Up);
-	}
+    public override void _Ready()
+    {
+        Camera ??= GetViewport().GetCamera3D();
+        Camera.LookAt(Center, Vector3.Up);
+    }
 
-	public override void _Input(InputEvent @event)
-	{
-		// If left mouse button is pressed while dragging the mouse
-		if (Camera != null && @event is InputEventMouseMotion mouseMotion && (int)mouseMotion.ButtonMask == (int)MouseButton.Left)
-		{
-			var camPostion = Camera.Position - Center;
-			// Rotate the camera around the center horizontally
-			camPostion = camPostion.Rotated(Vector3.Up, -mouseMotion.Relative.X * Sensitivity);
-			// Rotate the camera around the center vertically
-			var crossProduct = camPostion.Cross(Vector3.Up);
-			var newCamPosition = camPostion.Rotated(crossProduct.Normalized(), mouseMotion.Relative.Y * Sensitivity);
-			// Prevent the camera from going upside down
-			if (newCamPosition.Cross(Vector3.Up).Dot(crossProduct) > 0)
-				camPostion = newCamPosition;
-			Camera.LookAtFromPosition(Center + camPostion, Center);
-		}
-		else if (Camera != null && @event is InputEventMouseButton mouseWheel)
-		{
-			var camPostion = Camera.Position - Center;
-			// Zoom in and out
-			if (mouseWheel.ButtonIndex == MouseButton.WheelUp)
-			{
-				camPostion *= 0.95f;
-			}
-			else if (mouseWheel.ButtonIndex == MouseButton.WheelDown)
-			{
-				camPostion *= 1.05f;
-			}
-			else {
-				return;
-			}
-			Camera.LookAtFromPosition(Center + camPostion, Center);
-		}
-	}
+    public override void _Input(InputEvent @event)
+    {
+        // If left mouse button is pressed while dragging the mouse
+        if (Camera != null && @event is InputEventMouseMotion mouseMotion && (int)mouseMotion.ButtonMask == (int)MouseButton.Left)
+        {
+            var camPostion = Camera.Position - Center;
+            // Rotate the camera around the center horizontally
+            camPostion = camPostion.Rotated(Vector3.Up, -mouseMotion.Relative.X * Sensitivity);
+            // Rotate the camera around the center vertically
+            var crossProduct = camPostion.Cross(Vector3.Up);
+            var newCamPosition = camPostion.Rotated(crossProduct.Normalized(), mouseMotion.Relative.Y * Sensitivity);
+            // Prevent the camera from going upside down
+            if (newCamPosition.Cross(Vector3.Up).Dot(crossProduct) > 0)
+                camPostion = newCamPosition;
+            Camera.LookAtFromPosition(Center + camPostion, Center);
+        }
+        else if (Camera != null && @event is InputEventMouseButton mouseWheel)
+        {
+            var camPostion = Camera.Position - Center;
+            // Zoom in and out
+            if (mouseWheel.ButtonIndex == MouseButton.WheelUp)
+            {
+                camPostion *= 0.95f;
+            }
+            else if (mouseWheel.ButtonIndex == MouseButton.WheelDown)
+            {
+                camPostion *= 1.05f;
+            }
+            else {
+                return;
+            }
+            Camera.LookAtFromPosition(Center + camPostion, Center);
+        }
+    }
 }
