d0c92e11b73d5693074892a09bdf7f83a0c66853
Botond Zih <zihbot@gmail.com>
2023-10-22 15:13:39 +0200

    formatting

--- scenes/space/planetary_system/ClickableInputHandler.cs
+++ scenes/space/planetary_system/ClickableInputHandler.cs
@@ -3,33 +3,33 @@ using System;
 
 public partial class ClickableInputHandler : Node3D
 {
-	private Vector2? _lastClick = null;
+    private Vector2? _lastClick = null;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if (_lastClick == null)
-		{
-			return;
-		}
-		var camera = GetViewport().GetCamera3D();
-		var space = GetWorld3D().DirectSpaceState;
-		var start = camera.ProjectRayOrigin(_lastClick ?? Vector2.Zero);
-		var end = start + camera.ProjectRayNormal(_lastClick ?? Vector2.Zero) * camera.Far;
-		var query = PhysicsRayQueryParameters3D.Create(start, end, Constans.PHYSICS_LAYER_MASK_CLICKABLE);
-		_lastClick = null;
-		var collision = space.IntersectRay(query);
-		if (collision.Count == 0)
-		{
-			return;
-		}
-		//collision[0].As<CollisionShape3D>() Collider.GetParent().EmitSignal(nameof(Planet.PlanetClickedEventHandler));
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_lastClick == null)
+        {
+            return;
+        }
+        var camera = GetViewport().GetCamera3D();
+        var space = GetWorld3D().DirectSpaceState;
+        var start = camera.ProjectRayOrigin(_lastClick ?? Vector2.Zero);
+        var end = start + camera.ProjectRayNormal(_lastClick ?? Vector2.Zero) * camera.Far;
+        var query = PhysicsRayQueryParameters3D.Create(start, end, Constans.PHYSICS_LAYER_MASK_CLICKABLE);
+        _lastClick = null;
+        var collision = space.IntersectRay(query);
+        if (collision.Count == 0)
+        {
+            return;
+        }
+        //collision[0].As<CollisionShape3D>() Collider.GetParent().EmitSignal(nameof(Planet.PlanetClickedEventHandler));
+    }
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton mouseButton && mouseButton.IsPressed() && mouseButton.ButtonIndex == MouseButton.Left)
-		{
-			_lastClick = mouseButton.Position;
-		}
-	}
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseButton && mouseButton.IsPressed() && mouseButton.ButtonIndex == MouseButton.Left)
+        {
+            _lastClick = mouseButton.Position;
+        }
+    }
 }
--- scenes/space/planetary_system/PlanetarySystem.cs
+++ scenes/space/planetary_system/PlanetarySystem.cs
@@ -3,13 +3,13 @@ using System;
 
 public partial class PlanetarySystem : Node3D
 {
-	public override void _Ready()
-	{
-		var planet = GetChild<Planet>(0);
-		planet.PlanetClicked += () =>
-		{
-			var planetContext = GetNode<Control>("PlanetContext");
-			planetContext.Show();
-		};
-	}
+    public override void _Ready()
+    {
+        var planet = GetChild<Planet>(0);
+        planet.PlanetClicked += () =>
+        {
+            var planetContext = GetNode<Control>("PlanetContext");
+            planetContext.Show();
+        };
+    }
 }
