8f0294d9675d8a6a4a75ab23434a3604245edf16
Botond Zih <zihbot@gmail.com>
2023-10-25 18:07:29 +0200

    add result of function

--- resources/space/LaunchTrajectoryData.cs
+++ resources/space/LaunchTrajectoryData.cs
@@ -8,19 +8,24 @@ public partial class LaunchTrajectoryData : OrbitData
     {
         var aMax = 2;
 
-        var rMax = ReferencePoint.Length();
-        var rMin = CentralBody.Radius / 2; // Nem kell /2
-        var rDelta = rMax - rMin;
-
-        var r = rMin + rDelta * (fi/Mathf.Pi + 1f) / 2f;
-
-        var aRad = CentralBody.StandardGravitationalParameter / r / r;
-        var aTan = aMax - aRad;
-
         var speedStart = CentralBody.Radius * Mathf.Abs(ReferencePoint.Normalized().Dot(Vector3.Forward)) / CentralBody.RotationPeriod;
         var vStart = ReferencePoint.Normalized().Cross(Vector3.Down).Normalized() * speedStart / 1000;
         var vTarget = VelocityAtReferencePoint;
         var vDeltaTan = vTarget - vStart;
+
+        // t_m = sqrt(2 * v_tang / a_max)
+        var tM = Mathf.Sqrt(2 * vDeltaTan.Length() / aMax);
+
+        var rMax = ReferencePoint.Length();
+        var rMin = CentralBody.Radius / 2; // Nem kell /2
+        var rDelta = rMax - rMin;
+
+        var r = rMin + rDelta * (fi / Mathf.Pi + 1f) / 2f;
+
+        var aRad = CentralBody.StandardGravitationalParameter / r / r;
+        var aTan = aMax - aRad;
+
+
 
         // aTan is quadratic, so vDeltaTan is the quadratic integral of aTan, aMax^3/3
         // vDeltaTan = (aMax * tM^2 - aMax * t0) / 2 - mu * tM / h^2 - mu * tM^2 / (h^2 * t0)
