41350991c832841350c364eb7531a36b1d8dbcb2
Botond Zih <zihbot@gmail.com>
2023-10-29 18:25:39 +0100

    show orbit on click

--- scenes/space/orbit/Orbitter.cs
+++ scenes/space/orbit/Orbitter.cs
@@ -4,15 +4,31 @@ using System;
 public partial class Orbitter : Node3D
 {
     [Export] public OrbitData OrbitData;
+    [Export] public PackedScene OrbitScene { get; set; } = null;
 
     private SystemManager _systemManager;
     private Node3D _parent;
     private float trueAnomaly = 0;
+
     public override void _Ready()
     {
         _systemManager = GetNode<SystemManager>(SystemManager.TreeName);
         _parent = GetParent<Node3D>();
+        if (_parent is CelestialBody celestialBody)
+        {
+            celestialBody.Clicked += CelestialBodyClicked;
+        }
     }
+
+    private void CelestialBodyClicked()
+    {
+        if (OrbitScene == null) return;
+        var orbit = OrbitScene.Instantiate<Orbit>();
+        orbit.OrbitData = OrbitData;
+        _parent.GetParent().AddChild(orbit);
+        //orbit.QueueFree();
+    }
+
 
     public override void _Process(double delta)
     {
