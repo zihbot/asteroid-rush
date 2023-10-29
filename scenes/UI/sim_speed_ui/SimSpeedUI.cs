80df3fb549edcbd90cff2b762a9a186d302edde6
Botond Zih <zihbot@gmail.com>
2023-10-29 19:43:33 +0100

    shortcut pause

--- scenes/UI/sim_speed_ui/SimSpeedUI.cs
+++ scenes/UI/sim_speed_ui/SimSpeedUI.cs
@@ -16,8 +16,8 @@ public partial class SimSpeedUI : Control
     private long deltaTime = 0;
 
 
-	public override void _Ready()
-	{
+    public override void _Ready()
+    {
         s1 = GetNode<Button>("Grid/S1");
         s2 = GetNode<Button>("Grid/S2");
         s3 = GetNode<Button>("Grid/S3");
@@ -33,15 +33,51 @@ public partial class SimSpeedUI : Control
         s4.Disabled = true;
         s5.Disabled = true;
 
-        pause.Pressed += () => Engine.TimeScale = 0;
-        play.Pressed += () => Engine.TimeScale = 1;
-	}
+        pause.Pressed += () => SetSpeed(0);
+        play.Pressed += () => SetSpeed(1);
+    }
 
-	public override void _Process(double delta)
-	{
+    public override void _Process(double delta)
+    {
         sec += delta;
         deltaTime += Mathf.FloorToInt(sec);
         sec -= Mathf.Floor(sec);
         time.Text = $"{deltaTime}s";
-	}
+    }
+
+    private void ClearPressed()
+    {
+        //s1.ButtonPressed = false;
+        //s2.ButtonPressed = false;
+        //s3.ButtonPressed = false;
+        //s4.ButtonPressed = false;
+        //s5.ButtonPressed = false;
+        pause.ButtonPressed = false;
+        play.ButtonPressed = false;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        ClearPressed();
+        if (speed == 0)
+        {
+            pause.ButtonPressed = true;
+        }
+        else
+        {
+            play.ButtonPressed = true;
+        }
+        Engine.TimeScale = speed;
+    }
+
+    public override void _ShortcutInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("ui_sim_speed_pause_toggle"))
+        {
+            if (Engine.TimeScale == 0)
+                SetSpeed(1);
+            else
+                SetSpeed(0);
+        }
+    }
 }
