89e170ddb27b90b8069a5a4391c673017da84763
Botond Zih <zihbot@gmail.com>
2023-10-29 17:27:49 +0100

    format

--- scenes/space/orbit/Orbitter.cs
+++ scenes/space/orbit/Orbitter.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class Orbitter : Node3D
 {
-	[Export] public OrbitData OrbitData;
+    [Export] public OrbitData OrbitData;
 
-	private SystemManager _systemManager;
-	private Node3D _parent;
-	private float trueAnomaly = 0;
-	public override void _Ready()
-	{
-		_systemManager = GetNode<SystemManager>(SystemManager.TreeName);
-		_parent = GetParent<Node3D>();
-	}
+    private SystemManager _systemManager;
+    private Node3D _parent;
+    private float trueAnomaly = 0;
+    public override void _Ready()
+    {
+        _systemManager = GetNode<SystemManager>(SystemManager.TreeName);
+        _parent = GetParent<Node3D>();
+    }
 
-	public override void _Process(double delta)
-	{
+    public override void _Process(double delta)
+    {
         var deltaF = Mathf.PosMod((float)delta, Mathf.Pi * 2);
-		trueAnomaly = Mathf.Wrap(trueAnomaly + deltaF, -Mathf.Pi, Mathf.Pi);
-		if (_parent == null || !_parent.Visible || OrbitData == null) return;
+        trueAnomaly = Mathf.Wrap(trueAnomaly + deltaF, -Mathf.Pi, Mathf.Pi);
+        if (_parent == null || !_parent.Visible || OrbitData == null) return;
 
-		var position = OrbitData.PositionAtTrueAnomaly(trueAnomaly);
-		_parent.Position = position * _systemManager.SpaceScale;
-	}
+        var position = OrbitData.PositionAtTrueAnomaly(trueAnomaly);
+        _parent.Position = position * _systemManager.SpaceScale;
+    }
 }
