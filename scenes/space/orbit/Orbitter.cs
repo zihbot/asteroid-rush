4a1ef50f6f582d716fd9432bdc5d4fc21cdfd3e6
Botond Zih <zihbot@gmail.com>
2023-10-29 17:17:00 +0100

    Add orbitter

---
+++ scenes/space/orbit/Orbitter.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Orbitter : Node3D
+{
+	[Export] public OrbitData OrbitData;
+
+	private SystemManager _systemManager;
+	private Node3D _parent;
+	private float trueAnomaly = 0;
+	public override void _Ready()
+	{
+		_systemManager = GetNode<SystemManager>(SystemManager.TreeName);
+		_parent = GetParent<Node3D>();
+	}
+
+	public override void _Process(double delta)
+	{
+		trueAnomaly += (float)delta;
+		trueAnomaly = Mathf.Wrap(trueAnomaly, -Mathf.Pi, Mathf.Pi);
+		if (_parent == null || !_parent.Visible || OrbitData == null) return;
+
+		var position = OrbitData.PositionAtTrueAnomaly(trueAnomaly);
+		_parent.Position = position * _systemManager.SpaceScale;
+	}
+}
