7dbd56a1b36060eaf8d0b5bb9f855796340cb8a2
Botond Zih <zihbot@gmail.com>
2023-11-04 09:30:45 +0100

    update mission planning

--- scenes/UI/mission_planner/MissionPlanner.cs
+++ scenes/UI/mission_planner/MissionPlanner.cs
@@ -3,15 +3,29 @@ using System;
 
 public partial class MissionPlanner : Control
 {
+    [Export] public PackedScene OrbitScene { get; set; } = null;
     [Export] public Node Origin { get; set; } = null;
     [Export] public Node Target { get; set; } = null;
 
 	public override void _Ready()
 	{
-        if (Origin == null || Target == null)
+        if (Origin == null || Target == null || OrbitScene == null)
         {
             QueueFree();
             return;
         }
-	}
+
+        // Add LaunchTrajectory
+        Vector3 launchPad = new(0, 1, 2);
+        Vector3 targetSpeed = new(1, 0, 0);
+
+        LaunchTrajectoryData launch = new()
+        {
+            VelocityAtReferencePoint = targetSpeed
+        };
+
+        Orbit launchOrbit = OrbitScene.Instantiate<Orbit>();
+
+        Origin.AddChild(launchOrbit);
+    }
 }
